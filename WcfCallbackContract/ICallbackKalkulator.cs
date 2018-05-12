f684993a5cd7efc514f4e71b040bb64569bc2ce2
hkoscielski <huberto1010@op.pl>
2018-05-12 12:51:23 +0200

    Callback Contract

---
+++ WcfCallbackContract/ICallbackKalkulator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfCallbackContract
+{
+    [ServiceContract(SessionMode = SessionMode.Required,
+                    CallbackContract = typeof(ICallbackHandler))]
+    public interface ICallbackKalkulator
+    {
+        [OperationContract(IsOneWay = true)]
+        void Silnia(double n);
+        [OperationContract(IsOneWay = true)]
+        void ObliczCos(int sek);
+    }   
+
+    public interface ICallbackHandler
+    {
+        [OperationContract(IsOneWay = true)]
+        void ZwrotSilnia(double result);
+        [OperationContract(IsOneWay = true)]
+        void ZwrotObliczCos(string result);
+    }
+}
