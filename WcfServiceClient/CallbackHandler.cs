b12e93249c70a72b19030626e7d2805128179956
hkoscielski <huberto1010@op.pl>
2018-05-12 13:37:45 +0200

    Czesc I - A zrobiona

---
+++ WcfServiceClient/CallbackHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfServiceClient.ServiceReference2;
+
+namespace WcfCallbackContract
+{
+    class CallbackHandler : ICallbackKalkulatorCallback
+    {
+        public void ZwrotSilnia(double result)
+        {
+            //tu mozna wykorzystac result
+            Console.WriteLine(" Silnia = {0}", result);
+        }
+
+        public void ZwrotObliczCos(string info)
+        {
+            //tu mozna wykorzystac info
+            Console.WriteLine(" Obliczenia: {0}", info);
+        }
+    }
+}
--- WcfServiceClient/Program.cs
+++ WcfServiceClient/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using WcfCallbackContract;
 using WcfServiceClient.ServiceReference1;
+using WcfServiceClient.ServiceReference2;
 
 namespace WcfServiceClient
 {
@@ -12,6 +15,7 @@ namespace WcfServiceClient
     {
         static void Main(string[] args)
         {
+            //KLIENT 1
             // Krok 1: Utworzenie instancji WCF proxy.
             SerwisClient client1 = new SerwisClient("WSHttpBinding_ISerwis");
 
@@ -30,6 +34,29 @@ namespace WcfServiceClient
             Console.WriteLine("...kontynuacja po funkcji 1");
             client1.Close();
             Console.WriteLine("KONIEC KLIENT1");
+
+            //KLIENT 2
+            Console.WriteLine("\nKLIENT2");
+
+            CallbackHandler mojCallbackHandler = new CallbackHandler();
+            InstanceContext instanceContext = new InstanceContext(mojCallbackHandler);
+            CallbackKalkulatorClient client2 = new CallbackKalkulatorClient(instanceContext);
+
+            double value1 = 10;
+            Console.WriteLine("...wywoluje Silnia({0})...", value1);
+            client2.Silnia(value1);
+            value1 = 20;
+            Console.WriteLine("...wywoluje Silnia({0})...", value1);
+            client2.Silnia(value1);
+
+            int value2 = 2;
+            Console.WriteLine("...wywoluje obliczenia cosia...");
+            client2.ObliczCos(value2);
+            Console.WriteLine("...czekam chwile na odbior wynikow");
+            Thread.Sleep(5000);
+
+            client2.Close();
+            Console.WriteLine("KONIEC KLIENT2");
         }
     }
 }
