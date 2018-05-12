b916a974e43a94f09db233a316a801594a237412
hkoscielski <huberto1010@op.pl>
2018-05-12 12:02:41 +0200

    Dzialajacy host

---
+++ WcfServiceContract/MojSerwis.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+
+namespace WcfServiceContract
+{
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple)]
+    public class MojSerwis : ISerwis
+    {
+        void ISerwis.Funkcja(string s1)
+        {
+            Console.WriteLine("...{0}: funkcja1 - start", s1);
+            Thread.Sleep(2000);
+            Console.WriteLine("...{0}: funkcja1 - stop", s1);
+            return;
+        }
+
+        void ISerwis.Funkcja2(string s2)
+        {
+            Console.WriteLine("...{0}: funkcja2 - start", s2);
+            Thread.Sleep(2000);
+            Console.WriteLine("...{0}: funkcja2 - stop", s2);
+            return;
+        }
+    }
+}
--- WcfServiceHost/Program.cs
+++ WcfServiceHost/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
+using WcfServiceContract;
 
 namespace WcfServiceHost
 {
@@ -10,6 +13,34 @@ namespace WcfServiceHost
     {
         static void Main(string[] args)
         {
+            // Krok 1 Utworz URI dla bazowego adresu serwisu.
+            Uri baseAddress1 = new Uri("http://localhost:10004/MojSerwis");
+
+            // Krok 2 Utworz instancje serwisu.
+            ServiceHost mojHost1 = new ServiceHost(typeof(MojSerwis), baseAddress1);
+            try
+            {
+                // Krok 3 Dodaj endpoint.
+                ServiceEndpoint endpoint1 = mojHost1.AddServiceEndpoint(typeof(ISerwis), new WSHttpBinding(), "endpoint1");
+
+                // Krok 4 Ustaw wymiane metadanych.
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();               
+                smb.HttpGetEnabled = true;
+                mojHost1.Description.Behaviors.Add(smb);
+
+                // Krok 5 Uruchom serwis.
+                mojHost1.Open();
+                Console.WriteLine("--->MojSerwis jest uruchomiony.");
+                Console.WriteLine("--->Nacisnij <ENTER> aby zakonczyc.\n");
+                Console.ReadLine();
+                mojHost1.Close();
+                Console.WriteLine("---> Serwis zakonczyl dzialanie.");
+            }
+            catch(CommunicationException ce)
+            {
+                Console.WriteLine("Wystapil wyjatek: {0}", ce.Message);
+                mojHost1.Abort();
+            }
         }
     }
 }
